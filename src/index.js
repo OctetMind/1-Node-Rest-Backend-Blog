a0ead470adcc019dfd721b28dee0766eb41053d6
Paul <>
2022-01-02 17:13:18 +0200

    Adding paramters to the routes

--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ app.get('/', (reqest, response) => {
     response.send('Hello World!')
 })
 
-app.get('/Hello', (reqest,response) => {
+app.get('/Hello', (reqest, response) => {
     response.send('Oh, I didnt see you there')
 })
 
+app.get('/blog/:blogId', (req, response) => {
+    response.send(`Blog Id: ${req.params.blogId}`)
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
